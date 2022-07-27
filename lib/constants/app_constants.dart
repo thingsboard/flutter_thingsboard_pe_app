15969814c795a0eb50e6fb5dbfe36ee1adc66d33
Nehruprasath1 <75245302+Nehruprasath1@users.noreply.github.com>
2022-07-27 15:30:07 +0530

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'https://thingsboard.cloud';
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.pe.app.auth';
 
   /// Not for production (only for debugging)
