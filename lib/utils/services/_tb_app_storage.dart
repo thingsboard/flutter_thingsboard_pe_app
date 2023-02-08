6e1c69b9b6f95d74644512db8c6a2a1eaeee7933
Nathan-Smith-HL <nathansmith@hubblelithium.co.za>
2023-02-08 13:18:22 +0200

    Bug fixes

--- lib/utils/services/_tb_app_storage.dart
+++
@@ -1,3 +0,0 @@
-import 'package:thingsboard_pe_client/thingsboard_client.dart';
-
-TbStorage createAppStorage() => throw UnsupportedError('');
--- lib/utils/services/tb_app_storage.dart
+++
@@ -1,3 +0,0 @@
-export '_tb_app_storage.dart'
-    if (dart.library.io) '_tb_secure_storage.dart'
-    if (dart.library.html) '_tb_web_local_storage.dart';
