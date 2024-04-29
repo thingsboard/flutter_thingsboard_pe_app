550f051574c1a94b01d28b69543d8303b662a209
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-04-29 10:19:07 +0300

    Fixed an issue with login from custom endpoint.

--- lib/core/context/tb_context.dart
+++ lib/core/context/tb_context.dart
@@ -71,6 +71,7 @@ class TbContext implements PopEntry {
 
   TbMainDashboardHolder? _mainDashboardHolder;
   bool _closeMainFirst = false;
+  late bool _handleRootState;
 
   final ValueNotifier<bool> canPopNotifier = ValueNotifier<bool>(false);
 
@@ -110,7 +111,7 @@ class TbContext implements PopEntry {
       }
       return true;
     }());
-
+    _handleRootState = true;
     _initialized = true;
 
     final endpoint = await getIt<IEndpointService>().getEndpoint();
@@ -185,12 +186,13 @@ class TbContext implements PopEntry {
   }) async {
     log.debug('TbContext:reinit()');
 
+    _handleRootState = false;
     _initialized = false;
 
     tbClient = ThingsboardClient(
       endpoint,
       storage: getIt<ILocalDatabaseService>(),
-      onUserLoaded: () => onUserLoaded(handleRouteState: false, onDone: onDone),
+      onUserLoaded: () => onUserLoaded(onDone: onDone),
       onError: onError,
       onLoadStarted: onLoadStarted,
       onLoadFinished: onLoadFinished,
@@ -293,8 +295,7 @@ class TbContext implements PopEntry {
     _isLoadingNotifier.value = false;
   }
 
-  Future<void> onUserLoaded(
-      {bool handleRouteState = true, VoidCallback? onDone}) async {
+  Future<void> onUserLoaded({VoidCallback? onDone}) async {
     try {
       log.debug(
           'TbContext.onUserLoaded: isAuthenticated=${tbClient.isAuthenticated()}');
@@ -349,7 +350,7 @@ class TbContext implements PopEntry {
         onDone?.call();
       }
 
-      if (handleRouteState) {
+      if (_handleRootState) {
         await updateRouteState();
       }
 
@@ -422,6 +423,9 @@ class TbContext implements PopEntry {
     RequestConfig? requestConfig,
     bool notifyUser = true,
   }) async {
+    log.debug('TbContext::logout($requestConfig, $notifyUser)');
+    _handleRootState = true;
+
     if (getIt<IFirebaseService>().apps.isNotEmpty) {
       await NotificationService().logout();
     }
