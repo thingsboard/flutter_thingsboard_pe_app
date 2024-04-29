11a15aa1f7df8c1724eadf95da378d8baa3c9cf5
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-04-29 10:56:19 +0300

    TB demo qr code change to cloud.

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -15,6 +15,6 @@ abstract class ThingsboardImage {
     'github-logo': 'assets/images/github-logo.svg',
     'facebook-logo': 'assets/images/facebook-logo.svg',
     'apple-logo': 'assets/images/apple-logo.svg',
-    'qr-code-logo': 'assets/images/qr-demo-thingsboard.svg'
+    'qr-code-logo': 'assets/images/qr-cloud-thingsboard.svg'
   };
 }
