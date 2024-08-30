f1ab9adbe7401c5409c58c5964efa1aac7c24c7d
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-08-30 18:45:19 +0300

    Error Screen for Insufficient Dashboard Permissions

---
+++ lib/modules/dashboard/presentation/view/dashboard_permission_error_view.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:thingsboard_app/constants/assets_path.dart';
+import 'package:thingsboard_app/core/context/tb_context_widget.dart';
+import 'package:thingsboard_app/widgets/tb_app_bar.dart';
+
+class DashboardPermissionErrorView extends TbContextWidget {
+  DashboardPermissionErrorView(super.tbContext, {super.key});
+
+  @override
+  State<StatefulWidget> createState() => _DashboardPermissionErrorViewState();
+}
+
+class _DashboardPermissionErrorViewState
+    extends TbContextState<DashboardPermissionErrorView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TbAppBar(
+        tbContext,
+        leading: BackButton(onPressed: maybePop),
+        showLoadingIndicator: false,
+        elevation: 1,
+        shadowColor: Colors.transparent,
+        title: const FittedBox(
+          fit: BoxFit.fitWidth,
+          alignment: Alignment.centerLeft,
+          child: Text('Dashboard'),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              ThingsboardImage.dashboardPermissionError,
+              width: 101,
+              height: 82,
+            ),
+            const SizedBox(height: 16),
+            const Center(
+              child: Text(
+                'It looks like your permissions aren\'t '
+                'sufficient to complete this operation',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.25,
+                ),
+              ),
+            ),
+            const SizedBox(height: 82),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/services/permission/permission_service.dart
@@ -0,0 +1,17 @@
+import 'package:thingsboard_app/core/context/tb_context.dart';
+import 'package:thingsboard_app/thingsboard_client.dart';
+import 'package:thingsboard_app/utils/services/permission/i_permission_service.dart';
+
+class PermissionService implements IPermissionService {
+  @override
+  bool haveViewDashboardPermission(TbContext context) {
+    return context.hasGenericPermission(
+          Resource.WIDGETS_BUNDLE,
+          Operation.READ,
+        ) &&
+        context.hasGenericPermission(
+          Resource.WIDGET_TYPE,
+          Operation.READ,
+        );
+  }
+}
