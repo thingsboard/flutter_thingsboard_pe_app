4ed91af3e95f86e204c20385579fc5c77a8592dd
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-12-09 14:04:27 +0200

    TbContext refactoring.

--- lib/core/context/tb_context.dart
+++ lib/core/context/tb_context.dart
@@ -25,6 +25,8 @@ import 'package:thingsboard_app/utils/services/widget_action_handler.dart';
 import 'package:thingsboard_app/utils/services/wl_service.dart';
 import 'package:universal_platform/universal_platform.dart';
 
+part 'has_tb_context.dart';
+
 enum NotificationType { info, warn, success, error }
 
 class TbContext implements PopEntry {
@@ -726,117 +728,5 @@ class TbContext implements PopEntry {
         ],
       ),
     );
-  }
-}
-
-mixin HasTbContext {
-  late final TbContext _tbContext;
-
-  void setTbContext(TbContext tbContext) {
-    _tbContext = tbContext;
-  }
-
-  void setupCurrentState(TbContextState currentState) {
-    if (_tbContext.currentState != null) {
-      // ignore: deprecated_member_use
-      ModalRoute.of(_tbContext.currentState!.context)
-          ?.unregisterPopEntry(_tbContext);
-    }
-    _tbContext.currentState = currentState;
-    if (_tbContext.currentState != null) {
-      // ignore: deprecated_member_use
-      ModalRoute.of(_tbContext.currentState!.context)
-          ?.registerPopEntry(_tbContext);
-    }
-    if (_tbContext._closeMainFirst) {
-      _tbContext._closeMainFirst = false;
-      if (_tbContext.currentState != null) {
-        _tbContext.currentState!.closeMainFirst = true;
-      }
-    }
-  }
-
-  void setupTbContext(TbContextState currentState) {
-    _tbContext = currentState.widget.tbContext;
-  }
-
-  TbContext get tbContext => _tbContext;
-
-  TbLogger get log => _tbContext.log;
-
-  bool get isPhysicalDevice => _tbContext.isPhysicalDevice();
-
-  WidgetActionHandler get widgetActionHandler => _tbContext.widgetActionHandler;
-
-  ValueNotifier<bool> get loadingNotifier => _tbContext._isLoadingNotifier;
-
-  ThingsboardClient get tbClient => _tbContext.tbClient;
-
-  bool hasGenericPermission(Resource resource, Operation operation) =>
-      _tbContext.hasGenericPermission(resource, operation);
-
-  Future<void> initTbContext() async {
-    await _tbContext.init();
-  }
-
-  Future<dynamic> navigateTo(
-    String path, {
-    bool replace = false,
-    bool clearStack = false,
-  }) =>
-      _tbContext.navigateTo(path, replace: replace, clearStack: clearStack);
-
-  void pop<T>([T? result, BuildContext? context]) =>
-      _tbContext.pop<T>(result, context);
-
-  Future<void> navigateToDashboard(
-    String dashboardId, {
-    String? dashboardTitle,
-    String? state,
-    bool? hideToolbar,
-    bool animate = true,
-  }) =>
-      _tbContext.navigateToDashboard(
-        dashboardId,
-        dashboardTitle: dashboardTitle,
-        state: state,
-        hideToolbar: hideToolbar,
-        animate: animate,
-      );
-
-  Future<bool?> confirm({
-    required String title,
-    required String message,
-    String cancel = 'Cancel',
-    String ok = 'Ok',
-  }) =>
-      _tbContext.confirm(
-        title: title,
-        message: message,
-        cancel: cancel,
-        ok: ok,
-      );
-
-  void hideNotification() => _tbContext.hideNotification();
-
-  void showErrorNotification(String message, {Duration? duration}) =>
-      _tbContext.showErrorNotification(message, duration: duration);
-
-  void showInfoNotification(String message, {Duration? duration}) =>
-      _tbContext.showInfoNotification(message, duration: duration);
-
-  void showWarnNotification(String message, {Duration? duration}) =>
-      _tbContext.showWarnNotification(message, duration: duration);
-
-  void showSuccessNotification(String message, {Duration? duration}) =>
-      _tbContext.showSuccessNotification(message, duration: duration);
-
-  void subscribeRouteObserver(TbPageState pageState) {
-    _tbContext.routeObserver
-        .subscribe(pageState, ModalRoute.of(pageState.context) as PageRoute);
-  }
-
-  void unsubscribeRouteObserver(TbPageState pageState) {
-    _tbContext.routeObserver.unsubscribe(pageState);
   }
 }
