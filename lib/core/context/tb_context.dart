535c94b7ec3ccc29da366f97e84a53689a4abebe
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2025-04-02 14:34:58 +0300

    Revert "Fixed an issue When scanning the QR code from another server 2 times-a fatal error occurs, the user can no longer use the application."
    This reverts commit aa579f932a2f44708a08134ec22b5ff3bcf61942.

--- lib/core/context/tb_context.dart
+++ lib/core/context/tb_context.dart
@@ -207,6 +207,7 @@ class TbContext implements PopEntry {
         : 'Unknown error.';
     message = 'Fatal application error occured:\n$message.';
     await alert(title: 'Fatal error', message: message, ok: 'Close');
+    logout();
   }
 
   void onError(ThingsboardError tbError) {
@@ -341,6 +342,11 @@ class TbContext implements PopEntry {
             );
           } catch (e) {
             log.error('TbContext::onUserLoaded error $e');
+            if (!_isConnectionError(e)) {
+              logout();
+            } else {
+              rethrow;
+            }
           }
         }
       } else {
